d5bc7329c94d3fefaded296ee360391382ac8195
Fahri <fahricoskun354@outlook.com>
2024-07-21 22:52:33 +0300

    Refactor: Remove temporary files and update dependencies
    - Removed obsolete HTML, CSS, JS files and images from temp directory
    - Updated app.js with new changes
    - Updated package.json and package-lock.json for dependency adjustments
    - Added new public and views directories for project restructuring
    - Added notes.txt for documentation purposes

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middlewears
+app.use(express.static("public")); //! static dosyalarımı belirledik -> public klasörü
+
+//Routers
 app.get("/", (req, res) => {
-  res.status(200).send("Index Sayfasi"); //! http status codes mdn
+  res.status(200).render("index", {
+    page_name: "index", //! page_name
+  }); //! http status codes mdn
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
 });
 
 const port = 3000;
